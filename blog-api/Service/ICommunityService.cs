fcb1b82a6d58c21e8cc32f76e8d0aaabf0b601c6
Suslanium <zaharov22m@gmail.com>
2023-11-29 01:56:52 +0700

    Community post-related endpoints

--- blog-api/Service/ICommunityService.cs
+++ blog-api/Service/ICommunityService.cs
@@ -11,10 +11,15 @@ public interface ICommunityService
 
     public Task<CommunityFullDto> GetCommunityDetails(Guid communityGuid);
 
+    public Task<PostPagedListDto> GetCommunityPosts(Guid? userId, Guid communityId, List<Guid>? tags, string? authorName,
+        int? minReadingTime, int? maxReadingTime, SortingOption? sorting, int pageNumber, int pageSize);
+    
+    public Task CreatePost(Guid userId, Guid communityId, CreatePostDto postDto);
+
     public Task SubscribeUser(Guid userGuid, Guid communityGuid);
 
     public Task UnsubscribeUser(Guid userGuid, Guid communityGuid);
-    
+
     public Task AddAdministrator(Guid callerGuid, Guid userGuid, Guid communityGuid);
 
     public Task RemoveAdministrator(Guid callerGuid, Guid userGuid, Guid communityGuid);
